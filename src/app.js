3ca99837761bbf69fc6e9f67aff39e2e99cd11a2
sahithreddy05 <sahithreddy050101@gmail.com>
2024-12-07 03:42:50 -0500

    update

--- src/app.js
+++ src/app.js
@@ -18,6 +18,63 @@ app.post("/signup", async (req, res) => {
     }
 }); 
 
+// get user by email
+app.get("/user", async (req, res) => {
+    const email = req.body.emailId;
+    try{
+    const user = await User.find({emailId: email});
+
+        if(user.length === 0){
+            res.status(404).send("User not found");
+        }else{
+            res.send(user);
+        }
+    }
+    catch(err){
+        res.status(400).send("Error finding the user" + err.message)
+    }
+
+});
+
+// get all users
+app.get("/feed", async (req, res) => {
+
+    const email = req.body.emailId;
+    try{
+    const users = await User.find({});
+    res.send(users);
+    }
+    catch(err){
+        res.status(400).send("Error finding the user" + err.message)
+    }
+
+}
+);
+
+
+app.delete("/user", async (req, res) => {
+
+    const userId = req.body.userId;
+    try{
+        const user = await User.finfByIdAndDelete(userId);
+    }catch(err){
+        res.status(400).send("Error deleting the user" + err.message)
+    }
+});
+
+
+app.patch("/user", async (req, res) => {
+    
+        const userId = req.body.userId;
+        const data = req.body;
+        try{
+            const user = await User.findByIdAndUpdate(userId, data, );
+            res.send(user);
+        }catch(err){
+            res.status(400).send("Error deleting the user" + err.message)
+        }
+    }
+    );
 
 connectDB()
 .then(() => {
